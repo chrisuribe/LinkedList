9549f3606e6c977d80074c90e3263effe7f9a35e
Chris Uribe <chris.uribe.is@gmail.com>
2021-01-02 14:46:25 -0800

    Refactored indexOf. method is much shorter, variable names are clearer and more precise.

--- src/com/chrisuribe/LinkedList.java
+++ src/com/chrisuribe/LinkedList.java
@@ -4,12 +4,11 @@ public class LinkedList {
     private Node first;
     private Node last;
 
-    public void addLast(int value){
+    public void addLast(int value) {
         var node = new Node(value);
         if (isEmpty()) { // were starting a brand new LinkedList
             first = last = node;
-        }
-        else {
+        } else {
             last.next = node;
             last = node;
         }
@@ -19,20 +18,28 @@ public class LinkedList {
         var node = new Node(value);
         if (isEmpty()) { // were starting a brand new LinkedList
             first = last = node;
-        }
-        else {
+        } else {
             first = new Node(value, first);
         }
     }
 
-    private boolean isEmpty() { return first == null;}
-
-    private void startLinkedList(int value){
-        var node = new Node(value);
-        first = last = node;
+    private boolean isEmpty() {
+        return first == null;
     }
 
-    public void deleteFirst(){
+    public int indexOf(int item) {
+        int index = 0;
+        var current = first;
+        while (current != null) { // this would mean we reached the end
+            if(current.value == item) return index;
+            current = current.next;
+            index++;
+        }
+            return -1;
+    }
+
+
+    public void deleteFirst() {
         if (first == null) // empty LinkedList
             return;
         else {
@@ -48,14 +55,14 @@ public class LinkedList {
         else {
             Node newLast = first;
             // move pointer until "next" is equal to "last"
-            while(newLast.getNext() != last)
+            while (newLast.getNext() != last)
                 newLast = newLast.getNext();
             last = newLast;
         }
     }
 
-    public boolean contains (int value) {
-        if(last == null)
+    public boolean contains(int value) {
+        if (last == null)
             return false;
         else {
             Node targetNode = first;
@@ -66,40 +73,12 @@ public class LinkedList {
     }
 
 
-    public int indexOf (int value){
-        int index = 0;
-        if(last == null)
-            return -1;
-        else {
-            Node targetNode = first;
-            while (targetNode != last && targetNode.getValue() != value) {
-                index++;
-                targetNode = targetNode.getNext();
-            }
-            if (targetNode.getValue() == value)
-                return index;
-            else
-                return -1;
-        }
-    }
-
     // just a test
     private static class Node {
 
-        private int value;
+        private final int value;
         private Node next;
 
-
-        public int getValue() {
-            return value;
-        }
-        public Node getNext() {
-            return next;
-        }
-
-        public void setNext(Node next) {
-            this.next = next;
-        }
 
         public Node(int value) {
             this.value = value;
@@ -107,6 +86,18 @@ public class LinkedList {
 
         public Node(int value, Node next) {
             this.value = value;
+            this.next = next;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
             this.next = next;
         }
     }
